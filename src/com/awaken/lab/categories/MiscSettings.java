a67ba89464e40a758c3828b972627d1662e0e78e
Adin Kwok <adin.kwok@carbonrom.org>
2018-08-11 02:40:33 -0700

    SuperiorLab: Smart Pixels [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/awaken/lab/categories/MiscSettings.java
+++ src/com/awaken/lab/categories/MiscSettings.java
@@ -36,20 +36,43 @@ import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.util.awaken.systemUtils;
+import com.awaken.lab.fragments.SmartPixels;
 
+@SearchIndexable
 public class MiscSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
+
+    private static final String SMART_PIXELS = "smart_pixels";
+
+    private Preference mSmartPixels;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.awaken_lab_misc);
+
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mSmartPixels = (Preference) prefScreen.findPreference(SMART_PIXELS);
+        boolean mSmartPixelsSupported = getResources().getBoolean(
+                com.android.internal.R.bool.config_supportSmartPixels);
+        if (!mSmartPixelsSupported)
+            prefScreen.removePreference(mSmartPixels);
+    }
+
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+        SmartPixels.reset(mContext);
 
     }
 
@@ -63,4 +86,22 @@ public class MiscSettings extends SettingsPreferenceFragment implements
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.AWAKEN;
     }
+
+    /**
+     * For search
+     */
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.awaken_lab_misc) {
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+
+                    boolean mSmartPixelsSupported = context.getResources().getBoolean(
+                            com.android.internal.R.bool.config_supportSmartPixels);
+                    if (!mSmartPixelsSupported)
+                        keys.add(SMART_PIXELS);
+
+                    return keys;
+                }
+            };
 }
