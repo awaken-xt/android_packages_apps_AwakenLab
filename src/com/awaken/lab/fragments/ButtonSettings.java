480ebb340cfcb4e3ba3434124133d420c179df06
offender <98567050+offender6969@users.noreply.github.com>
2023-07-17 22:14:03 +0530

    Update ButtonSettings.java

--- src/com/awaken/lab/fragments/ButtonSettings.java
+++ src/com/awaken/lab/fragments/ButtonSettings.java
@@ -57,7 +57,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.awaken;
+        return MetricsProto.MetricsEvent.AWAKEN;
     }
 
 }
