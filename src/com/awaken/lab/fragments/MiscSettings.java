abhishekpeddada/android_packages_apps_SuperiorLab

SuperiorLab: Add random ImageHeader view [2/2]

src/com/superior/lab/fragments/MiscSettings.java
@@ -45,14 +45,17 @@ import java.util.List;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.superior.lab.fragments.SmartPixels;
+import com.android.internal.util.superior.systemUtils;
 
 @SearchIndexable
 public class MiscSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
     
     private static final String SMART_PIXELS = "smart_pixels";
+    private static final String SETTINGS_HEADER_IMAGE_RANDOM = "settings_header_image_random";
 
     private Preference mSmartPixels;
+    private Preference mSettingsHeaderImageRandom;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -61,6 +64,8 @@ public class MiscSettings extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.superior_lab_misc);
         final PreferenceScreen prefScreen = getPreferenceScreen();
         
+        mSettingsHeaderImageRandom = findPreference(SETTINGS_HEADER_IMAGE_RANDOM);
+        mSettingsHeaderImageRandom.setOnPreferenceChangeListener(this);
         mSmartPixels = (Preference) prefScreen.findPreference(SMART_PIXELS);
         boolean mSmartPixelsSupported = getResources().getBoolean(
                 com.android.internal.R.bool.config_supportSmartPixels);
@@ -75,6 +80,12 @@ public class MiscSettings extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+    	Context mContext = getActivity().getApplicationContext();
+	ContentResolver resolver = mContext.getContentResolver();
+	if (preference == mSettingsHeaderImageRandom) {
+            systemUtils.showSettingsRestartDialog(getContext());
+            return true;
+          }  
         return false;
     }
 

awaken-xt/android_packages_apps_AwakenLab

src/com/awaken/lab/fragments/MiscSettings.java
@@ -41,21 +41,32 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.util.awaken.systemUtils;
 
 public class MiscSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
+    private static final String SETTINGS_HEADER_IMAGE_RANDOM = "settings_header_image_random";
+
+    private Preference mSettingsHeaderImageRandom;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.awaken_lab_misc);
-
+        
+        mSettingsHeaderImageRandom = findPreference(SETTINGS_HEADER_IMAGE_RANDOM);
+        mSettingsHeaderImageRandom.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
+    	Context mContext = getActivity().getApplicationContext();
+	ContentResolver resolver = mContext.getContentResolver();
+	if (preference == mSettingsHeaderImageRandom) {
+            systemUtils.showSettingsRestartDialog(getContext());
+            return true;
+          }  
         return false;
     }
 