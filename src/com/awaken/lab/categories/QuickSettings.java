random-exp/packages_apps_BlazeHouse

BlazeHouse: Add QsTile Animations[2/2]

src/com/blaze/house/categories/QuickSettings.java
@@ -50,6 +50,9 @@ import java.util.Locale;
 import android.text.TextUtils;
 import android.view.View;
 
+import com.blaze.house.preferences.SystemSettingListPreference;
+import com.blaze.house.preferences.SystemSettingSeekBarPreference;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -59,11 +62,17 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
         
     private static final String KEY_QS_PANEL_STYLE = "qs_panel_style";
+    private static final String KEY_PREF_TILE_ANIM_STYLE = "qs_tile_animation_style";
+    private static final String KEY_PREF_TILE_ANIM_DURATION = "qs_tile_animation_duration";
+    private static final String KEY_PREF_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
 
     private Handler mHandler;
     private IOverlayManager mOverlayManager;
     private IOverlayManager mOverlayService;
     private SystemSettingListPreference mQsStyle;
+    private ListPreference mTileAnimationStyle;
+    private SystemSettingSeekBarPreference mTileAnimationDuration;
+    private ListPreference mTileAnimationInterpolator;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -79,6 +88,16 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
         mQsStyle = (SystemSettingListPreference) findPreference(KEY_QS_PANEL_STYLE);
         mCustomSettingsObserver.observe();
+        
+        mTileAnimationStyle = (ListPreference) findPreference(KEY_PREF_TILE_ANIM_STYLE);
+        mTileAnimationDuration = (SystemSettingSeekBarPreference) findPreference(KEY_PREF_TILE_ANIM_DURATION);
+        mTileAnimationInterpolator = (ListPreference) findPreference(KEY_PREF_TILE_ANIM_INTERPOLATOR);
+
+        mTileAnimationStyle.setOnPreferenceChangeListener(this);
+
+        int tileAnimationStyle = Settings.System.getIntForUser(resolver,
+                Settings.System.QS_TILE_ANIMATION_STYLE, 0, UserHandle.USER_CURRENT);
+        updateAnimTileStyle(tileAnimationStyle);
 
         }
         
@@ -111,8 +130,17 @@ public class QuickSettings extends SettingsPreferenceFragment implements
       if (preference == mQsStyle) {
                mCustomSettingsObserver.observe();
             return true;
-      }
+      } else if (preference == mTileAnimationStyle) {
+             int value = Integer.parseInt((String) newValue);
+             updateAnimTileStyle(value);
+             return true;
+          }
         return false;
+    }
+    
+    private void updateAnimTileStyle(int tileAnimationStyle) {
+        mTileAnimationDuration.setEnabled(tileAnimationStyle != 0);
+        mTileAnimationInterpolator.setEnabled(tileAnimationStyle != 0);
     }
     
     public static void reset(Context mContext) {

awaken-xt/android_packages_apps_AwakenLab

src/com/awaken/lab/categories/QuickSettings.java
@@ -56,6 +56,7 @@ import java.util.ArrayList;
 
 import com.awaken.support.preferences.SystemSettingListPreference;
 import com.awaken.support.preferences.SecureSettingMasterSwitchPreference;
+import com.awaken.support.preferences.SystemSettingSeekBarPreference;
 
 public class QuickSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
@@ -67,12 +68,18 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
     private static final String QS_PAGE_TRANSITIONS = "custom_transitions_page_tile";
     private static final String KEY_QS_PANEL_STYLE = "qs_panel_style";
+    private static final String KEY_PREF_TILE_ANIM_STYLE = "qs_tile_animation_style";
+    private static final String KEY_PREF_TILE_ANIM_DURATION = "qs_tile_animation_duration";
+    private static final String KEY_PREF_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
 
     private Handler mHandler;
     private IOverlayManager mOverlayManager;
     private IOverlayManager mOverlayService;
     private SystemSettingListPreference mQsStyle;
     private SystemSettingListPreference mPageTransitions;
+    private ListPreference mTileAnimationStyle;
+    private SystemSettingSeekBarPreference mTileAnimationDuration;
+    private ListPreference mTileAnimationInterpolator;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -110,6 +117,16 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
         mQsStyle = (SystemSettingListPreference) findPreference(KEY_QS_PANEL_STYLE);
         mCustomSettingsObserver.observe();
+        
+        mTileAnimationStyle = (ListPreference) findPreference(KEY_PREF_TILE_ANIM_STYLE);
+        mTileAnimationDuration = (SystemSettingSeekBarPreference) findPreference(KEY_PREF_TILE_ANIM_DURATION);
+        mTileAnimationInterpolator = (ListPreference) findPreference(KEY_PREF_TILE_ANIM_INTERPOLATOR);
+
+        mTileAnimationStyle.setOnPreferenceChangeListener(this);
+
+        int tileAnimationStyle = Settings.System.getIntForUser(resolver,
+                Settings.System.QS_TILE_ANIMATION_STYLE, 0, UserHandle.USER_CURRENT);
+        updateAnimTileStyle(tileAnimationStyle);
 
     }
     
@@ -165,8 +182,17 @@ public class QuickSettings extends SettingsPreferenceFragment implements
               } else if (preference == mQsStyle) {
                mCustomSettingsObserver.observe();
             return true;
+            } else if (preference == mTileAnimationStyle) {
+             int value = Integer.parseInt((String) newValue);
+             updateAnimTileStyle(value);
+             return true;
           } 
         return false;
+    }
+    
+    private void updateAnimTileStyle(int tileAnimationStyle) {
+        mTileAnimationDuration.setEnabled(tileAnimationStyle != 0);
+        mTileAnimationInterpolator.setEnabled(tileAnimationStyle != 0);
     }
     
     public static void reset(Context mContext) {