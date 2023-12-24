random-exp/packages_apps_BlazeHouse

BlazeHouse: Allow to change QS tile label text size [2/2]

src/com/blaze/house/fragments/QsTileLayoutSettings.java
@@ -41,11 +41,13 @@ import com.android.settingslib.widget.LayoutPreference;
 import com.blaze.house.preferences.CustomSeekBarPreference;
 import com.blaze.house.preferences.SystemSettingSwitchPreference;
 import com.blaze.house.preferences.SystemSettingListPreference;
+import com.blaze.house.preferences.SystemSettingSeekBarPreference;
 
 public class QsTileLayoutSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
     private static final String KEY_QS_HIDE_LABEL = "qs_tile_label_hide";
+    private static final String KEY_QS_LABEL_SIZE = "qs_tile_label_size";
     private static final String KEY_QS_VERTICAL_LAYOUT = "qs_tile_vertical_layout";
     private static final String KEY_QS_COLUMN_PORTRAIT = "qs_layout_columns";
     private static final String KEY_QS_ROW_PORTRAIT = "qs_layout_rows";
@@ -67,6 +69,7 @@ public class QsTileLayoutSettings extends SettingsPreferenceFragment
 
     private SystemSettingSwitchPreference mHide;
     private SystemSettingSwitchPreference mVertical;
+    private SystemSettingSeekBarPreference mSize;
 
     private int[] currentValue = new int[2];
 
@@ -135,6 +138,10 @@ public class QsTileLayoutSettings extends SettingsPreferenceFragment
 
         mVertical = (SystemSettingSwitchPreference) findPreference(KEY_QS_VERTICAL_LAYOUT);
         mVertical.setEnabled(!hideLabel);
+
+        mSize = (SystemSettingSeekBarPreference) findPreference(KEY_QS_LABEL_SIZE);
+        mSize.setEnabled(!hideLabel);
+
     }
 
     @Override
@@ -142,6 +149,7 @@ public class QsTileLayoutSettings extends SettingsPreferenceFragment
         if (preference == mHide) {
             boolean hideLabel = (Boolean) newValue;
             mVertical.setEnabled(!hideLabel);
+            mSize.setEnabled(!hideLabel);
         } else if (preference == mQsColumns) {
             int qs_columns = Integer.parseInt(newValue.toString());
             mApplyChange.setEnabled(

awaken-xt/android_packages_apps_AwakenLab

src/com/awaken/lab/fragments/QsTileLayoutSettings.java
@@ -41,11 +41,13 @@ import com.android.settingslib.widget.LayoutPreference;
 import com.awaken.support.preferences.CustomSeekBarPreference;
 import com.awaken.support.preferences.SystemSettingSwitchPreference;
 import com.awaken.support.preferences.SystemSettingListPreference;
+import com.awaken.support.preferences.SystemSettingSeekBarPreference;
 
 public class QsTileLayoutSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
     private static final String KEY_QS_HIDE_LABEL = "qs_tile_label_hide";
+    private static final String KEY_QS_LABEL_SIZE = "qs_tile_label_size";
     private static final String KEY_QS_VERTICAL_LAYOUT = "qs_tile_vertical_layout";
     private static final String KEY_QS_COLUMN_PORTRAIT = "qs_layout_columns";
     private static final String KEY_QS_ROW_PORTRAIT = "qs_layout_rows";
@@ -67,6 +69,7 @@ public class QsTileLayoutSettings extends SettingsPreferenceFragment
 
     private SystemSettingSwitchPreference mHide;
     private SystemSettingSwitchPreference mVertical;
+    private SystemSettingSeekBarPreference mSize;
 
     private int[] currentValue = new int[2];
 
@@ -135,6 +138,10 @@ public class QsTileLayoutSettings extends SettingsPreferenceFragment
 
         mVertical = (SystemSettingSwitchPreference) findPreference(KEY_QS_VERTICAL_LAYOUT);
         mVertical.setEnabled(!hideLabel);
+
+        mSize = (SystemSettingSeekBarPreference) findPreference(KEY_QS_LABEL_SIZE);
+        mSize.setEnabled(!hideLabel);
+
     }
 
     @Override
@@ -142,6 +149,7 @@ public class QsTileLayoutSettings extends SettingsPreferenceFragment
         if (preference == mHide) {
             boolean hideLabel = (Boolean) newValue;
             mVertical.setEnabled(!hideLabel);
+            mSize.setEnabled(!hideLabel);
         } else if (preference == mQsColumns) {
             int qs_columns = Integer.parseInt(newValue.toString());
             mApplyChange.setEnabled(